04f65624d1c5e5e306a35b83d53606e21b423bb8
Colin Sandman <xx56sandmanxx@gmail.com>
2021-03-10 12:11:11 -0500

    Creating Evidence Class stubs

--- EvidenceList.java
+++ EvidenceList.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class EvidenceList {
+public class EvidenceList extends Evidence{
     private ArrayList<String> evidence;
 
     public EvidenceList(ArrayList<String> evidence)
     {
+        super(UUID,String,String,UUID);
         evidence = new ArrayList<String>();
     }
 
-    public Evidence searchID(UUID evidenceID)
+    public Evidence searchID(Evidence evidenceID)
     {
         return evidenceID;
     }
