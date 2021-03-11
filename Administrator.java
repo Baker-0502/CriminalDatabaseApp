a7e6e9b678dae1ed78f789f1904b3658dbf89785
Baker-0502 <bakerdq@email.sc.edu>
2021-03-11 10:37:40 -0500

    Updated Adminstrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,22 +1,22 @@
-public class Administrator {
+public class Administrator extends User{
     private boolean updateCase;
     
-    public Administrator(boolean updateCase){
+    public Administrator(int userID, String firstName, String lastName, String username, String password, String email, String phoneNumber, String department, boolean updateCase){
+        super(userID, firstName, lastName, username, password, email, phoneNumber, department);
         this.updateCase = updateCase;
-       
     }
 
     public void addCase(String caseName){
-
+        System.out.print("Case Added!");
     }
     public void addCriminal(String criminalName){
-
+        System.out.print("Case Added!");
     }
     public void addPolice(String policeName){
-
+        System.out.print("Case Added!");
     }
     public void addDetective(String detectiveName){
-
+        System.out.print("Case Added!");
     }
     public void caseName(String caseName){
         caseName = "";
@@ -25,7 +25,7 @@ public class Administrator {
         updateCase = true;
     }
     public String toString(){
-        return policeName + criminalName + detectiveName + updateCase;
+        return getFirstName() + " " + getLastName() + ;
     }
 
     
