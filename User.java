d0b8e7b8f31a6a773345c4087f6a122467f9bc3b
Colin Sandman <xx56sandmanxx@gmail.com>
2021-03-11 11:01:07 -0500

    fixing User stubs

--- User.java
+++ User.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class User {
+public abstract class User {
     private int userID;
     private String firstName;
     private String lastName;
@@ -11,44 +11,44 @@ public class User {
     private String phoneNumber;
     private String department;
 
-public User(int userID, String firstName, String lastName, String username, String password, String email, String phoneNumber, String department)
-{
-    this.userID = userID;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.username = username;
-    this.password = password;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
-    this.department = department;
-}
+    public User(int userID, String firstName, String lastName, String username, String password, String email, String phoneNumber, String department)
+    {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.department = department;
+    }
 
-public void changePassword(String password) {
+    public void changePassword(String password) {
+        System.out.println("changing password");
+    }
 
-}
+    public void changeEmail(String email) {
+        System.out.println("changing email");
+    }
 
-public void changeEmail(String email) {
+    public void changePhoneNumber(String phoneNumber) {
+        System.out.println("changing phone number");
+    }
 
-}
+    public void changeDepartment(String department) {
+    System.out.println("chaging department");
+    }
 
-public void changePhoneNumber(String phoneNumber) {
+    public String getFirstName() {
+        return firstName;
+    }
 
-}
+    public String getLastName() {
+        return lastName;
+    }
 
-public void changeDepartment(String department) {
-    
-}
-
-public String getFirstName() {
-    return firstName;
-}
-
-public String getLastName() {
-    return lastName;
-}
-
-public String getLoginInfo() {
-    return username+password+email;
-}
+    public String getLoginInfo() {
+        return username+password+email;
+    }
 
 }
