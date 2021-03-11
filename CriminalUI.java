62066bea152ba3092861a66fbb8696a04c4bca35
Stokes <astokes121212@gmail.com>
2021-03-11 11:22:51 -0500

    added CriminalUI

---
+++ CriminalUI.java
@@ -0,0 +1,23 @@
+public class CriminalUI {
+    private CriminalDatabaseApplication application;
+
+    public void run(){
+        System.out.println("running");
+    }
+
+    public void displayLogin(){
+        System.out.println("displaying login");
+    }
+
+    public void displayMenu(){
+        System.out.println("displaying menu");
+    }
+
+    public void displayAddCase(){
+        System.out.println("adding case");
+    }
+
+    public void displayCases(){
+        System.out.println("displaying cases");
+    }
+}
