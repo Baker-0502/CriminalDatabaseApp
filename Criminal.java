9dbb5d07e1fc87458f5ea525e8ea8b3fa7ef519b
Baker-0502 <bakerdq@email.sc.edu>
2021-03-11 11:14:12 -0500

    Fixed Criminal Stub

--- Criminal.java
+++ Criminal.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 
-public class Criminal {
+public class Criminal extends Person{
     private String bloodType;
-    private String fingerprint;
+    private String fingerPrint;
     private String hairColor;
-    private ArrayList<Integer> clothing;
+    private ArrayList<String> clothing;
     private double footSize;
     private String eyeColor;
     private boolean isAlive;
-    private boolean tattoos;
+    private ArrayList<String> tattoos;
 
-public Criminal(String bloodType, String fingerprint, String hairColor, ArrayList<Integer> clothing, double footSize, String eyeColor, boolean isAlive, boolean tattoos)
+public Criminal(String firstName, String lastName, int age, double height, double weight, String phoneNumber, String address, String occupation, String bloodType, String fingerprint, String hairColor, ArrayList<String> clothing, double footSize, String eyeColor, boolean isAlive, ArrayList<String> tattoos)
 {
+    super(firstName, lastName, age, height, weight, phoneNumber, address, occupation);
     this.bloodType = bloodType;
-    this.fingerprint = fingerprint;
+    this.fingerPrint = fingerprint;
     this.hairColor = hairColor;
     this.clothing = clothing;
     this.footSize = footSize;
@@ -26,14 +27,14 @@ public String getBloodType() {
 }
 
 public String getFingerPrint() {
-    return fingerprint;
+    return fingerPrint;
 }
 
 public String getHairColor() {
     return hairColor;
 }
 
-public <ArrayList> getClothing() {
+public ArrayList<String> getClothing() {
     return clothing;
 }
 
@@ -50,11 +51,11 @@ public boolean isAlive(boolean isAlive) {
 }
 
 public String toString() {
-    return bloodType+fingerPrint+hairColor+clothing+footSize+eyeColor+isAlive+tatoos;
+    return bloodType+fingerPrint+hairColor+clothing+footSize+eyeColor+isAlive+tattoos;
 }
 
-private boolean tattoos(boolean tattoos) {
-    return tattoos;
+private ArrayList<String> getTattoos(){
+    return this.tattoos;
 }
 
 
