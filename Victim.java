46f8689886a41ee26568ff096e25c26ff54a4e25
Baker-0502 <bakerdq@email.sc.edu>
2021-03-15 22:35:36 -0400

    Further File Updates

--- Victim.java
+++ Victim.java
@@ -1,9 +1,10 @@
-public class Victim {
+public class Victim extends Person{
     private String relationship;
     private String statement;
 
-public Victim(String relationship, String statement)
+public Victim(String firstName, String lastName, int age, double height, double weight, String phoneNumber, String address, String occupation, String relationship, String statement)
 {
+    super(firstName, lastName, age, height, weight, phoneNumber, address, occupation);
     this.relationship = relationship;
     this.statement = statement;
 }
--- Witness.java
+++ Witness.java
@@ -1,9 +1,10 @@
-public class Witness {
+public class Witness extends Person{
     private String relationship;
     private String statement;
 
-public Witness(String relationship, String statement)
+public Witness(String firstName, String lastName, int age, double height, double weight, String phoneNumber, String address, String occupation, String relationship, String statement)
 {
+    super(firstName, lastName, age, height, weight, phoneNumber, address, occupation);
     this.relationship = relationship;
     this.statement = statement;
 }
