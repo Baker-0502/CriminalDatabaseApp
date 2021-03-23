2b0a41b683277ea8a9fc6d85db9241051176f0c6
Kevin <kn11@email.sc.edu>
2021-03-23 11:17:47 -0400

    Class text and space change

--- policeOfficer.java
+++ policeOfficer.java
@@ -1,46 +1,37 @@
 import java.util.UUID;
 
-public class policeOfficer extends User{
+public class policeOfficer extends User {
     private String badgeID;
     private int caseCount;
     private boolean editAccess;
    
 
-    public policeOfficer(UUID userID, String firstName, String lastName, String username, String password, String email, String phoneNumber, String department, String badgeID, int caseCount, boolean editAccess)
-    {
+    public policeOfficer(UUID userID, String firstName, String lastName, String username, String password, String email, String phoneNumber, String department, String badgeID, int caseCount, boolean editAccess) {
         super(userID, firstName, lastName, username, password, email, phoneNumber, department);
         this.badgeID = badgeID;
         this.caseCount = caseCount;
         this.editAccess = editAccess;        
     }
 
-    public void addWitness(String witness)
-    {
-       System.out.println("adding witness");
+    public void addWitness(String witness) {
+       System.out.println("Adding Witness");
     }
-
-    public void addAssociate(String associates)
-    {
-        System.out.println("adding associate");
+    public void addAssociate(String associates) {
+        System.out.println("Adding Associate");
     }
-    public void addSuspect(String suspect)
-    {
-        System.out.println("adding suspect");
+    public void addSuspect(String suspect) {
+        System.out.println("Adding Suspect");
     }
-    public void addVictim(String victim)
-    {
-        System.out.println("adding victim");
+    public void addVictim(String victim) {
+        System.out.println("Adding Victim");
     }
-    public void addEvidence(String evidence)
-    {
-        System.out.println("adding evidence");
+    public void addEvidence(String evidence) {
+        System.out.println("Adding Evidence");
     }
-    public void addNumOfCases(int caseCount)
-    {
+    public void addNumOfCases(int caseCount) {
         caseCount = 0;
     }
-    public void requestEdit(boolean editAccess)
-    {
+    public void requestEdit(boolean editAccess) {
         editAccess = false;
     }
     public String getBadgeID() {
@@ -52,7 +43,7 @@ public class policeOfficer extends User{
     public boolean getEditAccess() {
         return editAccess;
     }
-    public String toString(){
+    public String toString() {
         return super.toString()+"\nBadge ID: "+badgeID+"\nCase Count: "+caseCount+"\nEdit Access: "+editAccess+"\n";
     }
    
