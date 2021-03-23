a3d80ddcac42ae71f38f37aca6166840b6b4e975
Kevin <kn11@email.sc.edu>
2021-03-23 11:25:45 -0400

    Minor Fixes to Detective

--- Detective.java
+++ Detective.java
@@ -1,26 +1,26 @@
 import java.util.UUID;
 
-public class Detective extends User{
+public class Detective extends User {
     private String associate;
 
-    public Detective(String associate, String department, UUID userID, String firstName, String lastName, String username, String password, String email, String phoneNumber){
+    public Detective(String associate, String department, UUID userID, String firstName, String lastName, String username, String password, String email, String phoneNumber) {
         super(userID, firstName, lastName, username, password, email, phoneNumber, department);
         this.associate = associate;
     }
 
-    public void addWitnessStatement(String witnessStatement){
+    public void addWitnessStatement(String witnessStatement) {
         System.out.println("Witness Statement: " + witnessStatement + " added.");
     }
 
-    public void addAssociate(String associate){
+    public void addAssociate(String associate) {
         System.out.println("Associate: " + associate + " added.");
     }
 
-    public void addReport(String report){
+    public void addReport(String report) {
         System.out.println("Report: " + report + " added.");
     }
 
-    public void addEvidence(String evidence){
+    public void addEvidence(String evidence) {
         System.out.println("Evidence: " + evidence + " added.");
     }
 
@@ -28,7 +28,7 @@ public class Detective extends User{
         return associate;
     }
 
-    public String toString(){
-        return super.toString()+"\nAssociate: "+associate+"\n";
+    public String toString() {
+        return super.toString()+"\nAssociate: "+ associate + "\n";
     }
 }
