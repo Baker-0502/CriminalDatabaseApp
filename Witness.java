21cd9963f1b1942d4f3cc4dd710a098cf2e2610e
Stokes <astokes121212@gmail.com>
2021-03-28 21:01:32 -0400

    witness doc

--- Witness.java
+++ Witness.java
@@ -5,28 +5,10 @@ import java.util.UUID;
 public class Witness extends Person {
     private String relationship;
     private String statement;
-<<<<<<< HEAD
-/**
- * details about the witness
- * @param personID id placed for specific witness
- * @param firstName witness firstname
- * @param lastName witness lastname
- * @param gender gender of the witness
- * @param race witness race
- * @param age age of the witness
- * @param height witness height
- * @param weight weight of the witness
- * @param phoneNumber phone number of the witness
- * @param address witness address
- * @param occupation occupation of witness if avaliable
- * @param relationship relationship of witness to case
- * @param statement witness statement
-=======
 
 
 /**
  * default constructor for a witness
->>>>>>> 3743ceee16820250dfa230f20f89d75c03ae2b0d
  */
 public Witness(UUID personID, String firstName, String lastName, String gender, String race, int age, double height, double weight, String phoneNumber, String address, String occupation, String relationship, String statement)
 {
@@ -34,41 +16,23 @@ public Witness(UUID personID, String firstName, String lastName, String gender, 
     this.relationship = relationship;
     this.statement = statement;
 }
-<<<<<<< HEAD
-/**
- * relation of the witness to case
- * @return return that relationship
-=======
 
 /**
  * returning the relation of the witness to the criminal
->>>>>>> 3743ceee16820250dfa230f20f89d75c03ae2b0d
  */
 public String getRelationship() {
     return relationship;
 }
-<<<<<<< HEAD
-/**
- * gets the statement of the witness
- * @return returns the statement of the witness
-=======
 
 /**
  * returns the witness statment
->>>>>>> 3743ceee16820250dfa230f20f89d75c03ae2b0d
  */
 public String getStatement() {
     return statement;
 }
-<<<<<<< HEAD
-/**
- * tostring outputs details of the witness
- * @return outputs the relationship of witness to case and the statment made by the witness
-=======
 
 /**
  * prints out the relationship and statement
->>>>>>> 3743ceee16820250dfa230f20f89d75c03ae2b0d
  */
 public String toString() { 
     return super.toString() + "\nRelationship: "+relationship+"\nStatement: "+statement+"\n";
